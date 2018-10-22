6440affb5a2562f16795f40cd2e1d10269beb40f
leena-gokhale <gokhale.leena@gmail.com>
2018-10-22 12:23:04 +0530

    -- clean up cloud functions related code. We don't need it anymore. (Keet in git history as a record.)

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import CreateEvent from './Components/CreateEvent';
 import EventsHome from './Components/EventsHome';
 import RegisterForEvent from './Components/RegisterForEvent';
 import ListForEvent from './Components/ListForEvent';
-import SendEmail from './Components/SendEmail';
 
 import { createStackNavigator } from 'react-navigation';
 
@@ -21,7 +20,6 @@ const NewRootStack = createStackNavigator({
   CreateEvent: { screen: CreateEvent },
   RegisterForEvent: { screen: RegisterForEvent },
   ListForEvent: {screen: ListForEvent},
- //SendEmail: {screen: SendEmail}
 },
   {
     initialRouteName: 'EventsHome',
